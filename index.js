ac1768718b2ad5ccccf0f7e261ab031e48cfc852
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-08-03 15:31:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+ const employee = {
+    name: "geoffrey",
+    streetAddress: "12334 Nairobi",
+ }
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+     return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+     const newEmployee = {...employee};
+     delete newEmployee[key];
+     return newEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+     delete employee[key];
+     return employee;
+}
